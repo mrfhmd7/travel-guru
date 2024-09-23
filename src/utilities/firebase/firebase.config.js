2b65b84bf457f08151b2d75fbf6363d80a8fd45f
mrfhmd7 <mrf.hmd7@gmail.com>
2024-09-23 22:41:27 +0600

    initial commit

---
+++ src/utilities/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCOR7skZb8nVTM5DU_ppPZcbcK_s9bTloY",
+  authDomain: "travel-guru-mrf-hmd7.firebaseapp.com",
+  projectId: "travel-guru-mrf-hmd7",
+  storageBucket: "travel-guru-mrf-hmd7.appspot.com",
+  messagingSenderId: "640380210889",
+  appId: "1:640380210889:web:09a967b8f0a95370a98718"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
