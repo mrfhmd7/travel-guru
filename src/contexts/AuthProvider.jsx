dbecf353aa78c8fe8937f7c324ec1bf0717ec31d
mrfhmd7 <mrf.hmd7@gmail.com>
2024-09-25 22:38:26 +0600

    Authprovider created

--- src/contexts/AuthProvider.jsx
+++ src/contexts/AuthProvider.jsx
@@ -1,11 +1,59 @@
-import React from 'react';
+import React, { createContext, useEffect, useState } from 'react';
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signInWithPopup, GoogleAuthProvider, signOut, onAuthStateChanged } from 'firebase/auth';
+import app from '../utilities/firebase/firebase.config'
 
-const AuthProvider = () => {
-     return (
-          <div>
-               
-          </div>
-     );
+export const AuthContext = createContext();
+
+const auth = getAuth(app);
+const googleAuthProvider = new GoogleAuthProvider();
+
+const AuthProviders = ({ children }) => {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    const createUser = (email, password) => {
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password);
+    };
+
+    const signIn = (email, password) => {
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password);
+    };
+
+    const signInWithGoogle = () => {
+        return signInWithPopup(auth, googleAuthProvider);
+    };
+
+    const logOut = () => {
+        return signOut(auth);
+    };
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+            // console.log(currentUser);
+            setUser(currentUser);
+            setLoading(false);
+        });
+
+        return () => unsubscribe();
+    }, []);
+
+    const authInfo = {
+        user,
+        loading,
+        createUser,
+        signIn,
+        signInWithGoogle,
+        logOut,
+        auth,
+    };
+
+    return (
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
+    );
 };
 
-export default AuthProvider;
+export default AuthProviders;
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import routes from './routes/routes/Routes.jsx'
+import AuthProviders from './contexts/AuthProvider.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={routes}></RouterProvider>
+    <AuthProviders>
+      <RouterProvider router={routes}></RouterProvider>
+    </AuthProviders>
   </StrictMode>,
 )
