be44975ad7eea817b92b32b7f4ea85b7539cbd23
mrfhmd7 <mrf.hmd7@gmail.com>
2024-09-28 22:40:16 +0600

    login in with email

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,13 +1,62 @@
-import { useContext, useState } from 'react';
+import React, { useContext, useRef, useState } from 'react';
 import { FaFacebookF, FaGoogle } from 'react-icons/fa';
 import { AuthContext } from '../../contexts/AuthProvider';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { sendPasswordResetEmail } from 'firebase/auth';
 
 const Login = () => {
      const [rememberMe, setRememberMe] = useState(false);
-     const { signInWithGoogle, } = useContext(AuthContext);
+     const emailRef = useRef();
+     const { signIn, auth, signInWithGoogle, } = useContext(AuthContext);
+     const [show, setShow] = useState(false);
+     const navigate = useNavigate();
+     const location = useLocation();
+     console.log(location);
+     const from = location.state?.from?.pathname || "/";
 
-     const handleLogin = (e) => {
-          e.preventDefault();
+     const handleLogin = event => {
+          event.preventDefault();
+
+          const form = event.target;
+          const email = form.email.value;
+          const password = form.password.value;
+
+          signIn(email, password)
+               .then(result => {
+                    const loggedUser = result.user;
+                    console.log(loggedUser);
+
+                    if (!loggedUser.emailVerified) {
+                         toast.warning('Your email is not verified, please verify your email');
+                         return;
+                    }
+
+                    toast.success('Account login successful');
+                    form.reset();
+                    navigate(from, { replace: true });
+               })
+               .catch(error => {
+                    console.error(error);
+                    toast.error('Error login Account');
+               })
+     };
+
+     const handleResetPassword = () => {
+          const email = emailRef.current.value;
+          if (!email) {
+               toast.warning('Please enter your email for reset password');
+               return;
+          }
+
+          sendPasswordResetEmail(auth, email)
+               .then(() => {
+                    toast.success('Please check your email');
+               })
+               .catch(error => {
+                    console.log(error.message);
+               })
      };
 
      const handleGoogleLogin = () => {
@@ -15,11 +64,11 @@ const Login = () => {
                .then(result => {
                     const loggedUser = result.user;
                     console.log(loggedUser);
-                    // toast.success('Signed in with Google successfully!');
+                    toast.success('Signed in with Google successfully!');
                })
                .catch(error => {
                     console.log(error);
-                    // toast.error('Error signing in with Google');
+                    toast.error('Error signing in with Google');
                })
      };
 
@@ -36,6 +85,7 @@ const Login = () => {
                                    type="email"
                                    placeholder="email"
                                    name="email"
+                                   ref={emailRef}
                                    className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring focus:ring-yellow-300"
                                    required
                               />
@@ -60,9 +110,9 @@ const Login = () => {
                                    />
                                    <span className="ml-2 text-sm text-gray-700">Remember Me</span>
                               </label>
-                              <a href="#" className="text-sm text-yellow-500 hover:underline">
+                              <button onClick={handleResetPassword} className="text-sm text-yellow-500 hover:underline">
                                    Forgot Password?
-                              </a>
+                              </button>
                          </div>
                          <div>
                               <button
@@ -87,6 +137,9 @@ const Login = () => {
                               <span className="px-2 bg-white text-gray-500">Or</span>
                          </div>
                     </div>
+
+                    <ToastContainer />
+
                     <div className="grid gap-4">
                          <button className="flex items-center justify-center px-4 py-2 text-sm font-medium border border-blue-400 text-blue-500 rounded-full hover:bg-blue-500 hover:text-white">
                               <FaFacebookF className="mr-2" /> Continue with Facebook
